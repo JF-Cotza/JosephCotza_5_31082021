ceb13152b7350b275f2b7ab74e2d2f6e26011d2e
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-01 00:09:56 +0200

    ajout js

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
+//fonctions et constantes
+const getId= ($value) =>{
+   return document.getElementById($value);
+}
+
+const toggle= ($cible,$class)=>{
+    $cible.classList.toggle($class);
+}
+
+
+//variables
+let navbar = getId('navbarContent')
+
+
+
+navbar.addEventListener('click',function(){
+    toggle(navbar, 'navbar-collapse');
+})
+
 fetch("http://localhost:3000/api/teddies");
+
+
