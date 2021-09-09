8a168dc28cc1e9f77cf7aa35ad588c76a78c6a07
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-09 23:50:41 +0200

    travail sur template

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,10 +1,18 @@
-const apiLink= 'localhost:3000/api/teddy'; //2erreurs
-//const apiLink= 'localhost:3000/api/teddies'; //2erreurs
-//const apiLink= 'localhost/api/teddy' //3erreurs
 
+const apiLink = 'http://localhost:3000/api/teddies';
+const storage= ($nom, $value)=> localStorage.setItem($nom,$value)
+const getItem= ($nom)=>localStorage.getItem($nom)
 
 fetch(apiLink)
-    .then((res) => { console.log(res);})
+    .then((res) => {
+                        let value=res[0];
+                        console.log('longueur: '+value.length);
+                        for (let i=0; i<res.length; i++){
+                            console.log(i+' '+res[i]);
+                        }
+                        
+                    })
     .catch((error)=> 
         {console.log(error.message + ' erreur du fetch ');}
         );
+
