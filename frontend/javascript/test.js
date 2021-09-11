26a5d20fb242ee5549444acc1ddf837f56f1ae0f
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-11 15:31:30 +0200

    copie de teddy dans test.js

--- frontend/javascript/test.js
+++ frontend/javascript/test.js
@@ -1,8 +1,56 @@
-let colors=['rouge','vert','noir','bleu'];
-let objetest1={color:colors,id:'id-id1',name:'objet1',price:5400,imageurl:'testimage-2000x2000.png',description:'loremjdvnsvslbnflknbflkbnlfbnlkfnblf,blf,bmlfdlnbnldblndlbk'};
-let objetest2={ color:colors, id: 'id-id2', name:'objet2', price: 5400, imageurl: 'testimage-2000x2000.png', description: 'loremjdvnsvslbnflknbflkbnlfbnlkfnblf,blf,bmlfdlnbnldblndlbk' };
-let objetest3= { color:colors, id: 'id-id3', name:'objet3', price: 5400, imageurl: 'testimage-2000x2000.png', description: 'loremjdvnsvslbnflknbflkbnlfbnlkfnblf,blf,bmlfdlnbnldblndlbk' };
-let objetest4= { color:colors, id: 'id-id4', name:'objet4', price: 5400, imageurl: 'testimage-2000x2000.png', description: 'loremjdvnsvslbnflknbflkbnlfbnlkfnblf,blf,bmlfdlnbnldblndlbk' };
-
-let objetest=[objetest1,objetest2,objetest3,objetest4]
-console.log(objetest);
+const teddies = [
+    {
+        "colors": ["Tan", "Chocolate", "Black", "White"],
+        "_id": "5be9c8541c9d440000665243",
+        "name": "Norbert",
+        "price": 2900,
+        "imageUrl": "teddy_1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "colors": [
+            "Pale brown",
+            "Dark brown",
+            "White"
+        ],
+        "_id": "5beaa8bf1c9d440000a57d94",
+        "name": "Arnold",
+        "price": 3900,
+        "imageUrl": "teddy_2.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "colors": [
+            "Brown"
+        ],
+        "_id": "5beaaa8f1c9d440000a57d95",
+        "name": "Lenny and Carl",
+        "price": 5900,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "imageUrl": "teddy_3.jpg"
+    },
+    {
+        "colors": [
+            "Brown",
+            "Blue",
+            "Pink"
+        ],
+        "_id": "5beaabe91c9d440000a57d96",
+        "name": "Gustav",
+        "price": 4500,
+        "imageUrl": "teddy_4.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "colors": [
+            "Beige",
+            "Tan",
+            "Chocolate"
+        ],
+        "_id": "5beaacd41c9d440000a57d97",
+        "name": "Garfunkel",
+        "price": 5500,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "imageUrl": "teddy_5.jpg"
+    }
+];
