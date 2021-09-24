b8f471c542d118d52d8a091d71ef2430f4dd9960
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-24 22:46:27 +0200

    extraction de fonctions dans script template

--- frontend/javascript/script_template.js
+++ frontend/javascript/script_template.js
@@ -6,46 +6,14 @@ const index=(param_fetchdata)=>{
     //on explore les produits
     for (let produit of param_fetchdata) {
         let copy = clone(document, template);
-        let url = select(copy, '.cardLink')[0];
-        let name = select(copy, '.cardModelName')[0];
-        let option = select(copy, '.cardOption')[0];
-        let price = select(copy, '.cardPrice')[0];
-        let detail = select(copy, '.cardDetails')[0];
-        let id = select(copy, '.cardId')[0];
-        let image = document.createElement('img');
-
-        image.setAttribute('alt', produit.alt);
-        image.setAttribute('src', produit.imageUrl);
-        url.href = `product.html?id=${produit._id}`;
-        name.textContent = 'modèle: '+produit.name;        
-        price.textContent = 'prix: '+(produit.price) / 100 + '€';
-        detail.textContent = 'description: '+produit.description;
-        id.textContent = produit._id;
-        
-        if(type=='teddies'){
-            option.textContent = 'couleurs: '+ produit.colors;
-        }
-        else if (type=='cameras'){
-            option.textContent = 'objectifs: '+produit.lenses;
-        }
-        else if (type=='furniture'){
-            option.textContent = 'vernis: '+produit.varnish;
-
-        }
-
-        
-        url.appendChild(image); //on crée l'image dans le l'url
-        body.appendChild(copy); //on crée la carte produit
-
+        productData(copy,produit,body);          
     }
 }
 
-function product(param_fetchdata,param_idproduct) {
-    let showingProduct = { 'id': '', 'name': '','price':'','option': [] }
-
+const product=(param_fetchdata,param_idproduct)=> {
     //on sélectionne le template par son ID
     let template = document.querySelector('#productTemplate');
-    let howmany=document.querySelector('#howmany');
+
     //on sélectionne là où on on créera les cartes.
     let body = select(document, '#productCommand')[0];
     let count = 0;
@@ -56,90 +24,11 @@ function product(param_fetchdata,param_idproduct) {
         let copy = clone(document, template);
         
         if(produit._id==param_idproduct){
-            //let url = select(copy, '.cardLink')[0];
-            let name = select(copy, '.cardModelName')[0];
-            let option = select(copy, '.cardOptionSelect')[0];
-            let price = select(copy, '.cardPrice')[0];
-            let detail = select(copy, '.cardDetails')[0];
-            let id = select(copy, '.cardId')[0];
-            let image=select(copy,'.cardImage')[0];
-            let imageAlt='';
-            let i=0;
-
-            image.setAttribute('src', produit.imageUrl);
-            name.textContent = produit.name;
-            price.textContent = (produit.price)/100;
-            detail.textContent = 'description: ' + produit.description;
-            id.value = produit._id;
-            
-
-            let tableOption=[];
-            if (type == 'teddies') {
-                imageAlt='un ours en peluche';
-                for (let colors of produit.colors){
-                    let selectoption=document.createElement('option');
-                    selectoption.value=i;
-                    selectoption.textContent=colors;
-                    option.appendChild(selectoption);
-                    tableOption.push({'color':colors,'quantity':''});
-                    i++;
-                }
-            }
-            else if (type == 'cameras') {
-                imageAlt='un appareil photo'
-                for (let lense of produit.lenses) {
-                    let selectoption = document.createElement('option');
-                    selectoption.value = i;
-                    selectoption.textContent = lense;
-                    option.appendChild(selectoption);
-                    tableOption.push({ 'Lense': lense, 'quantity': '' });
-                    i++;
-                }
-            }
-            else if (type == 'furniture') {
-                option.textContent = 'vernis: ' + produit.varnish;
-                imageAlt='un meuble';
-                for (let varnishOption of produit.varnish) {
-                    let selectoption = document.createElement('option');
-                    selectoption.value = i;
-                    selectoption.textContent = varnishOption;
-                    option.appendChild(selectoption);
-                    tableOption.push({ 'Vernis': varnishOption, 'quantity': '' });
-                    i++;
-                }
-            }
-
-            showingProduct.price=produit.price;
-            showingProduct.name=produit.name;
-            showingProduct.id = produit.id;
-            showingProduct.option=tableOption;
-            
-            image.setAttribute('alt', imageAlt);
-            body.appendChild(copy); //on crée la carte produit
-        
-
+            productShowing(copy,produit,body);        
         
             //on regarde si une option est sélectionnée
             option.addEventListener('change',function(){
-                let volume=clone(document,howmany);
-                let label = document.createElement('Label');
-                let input = document.createElement('input');
-                let commande = getClass('commande')[0];
-
-                //définition du label
-                label.setAttribute('for', produit.colors[option.value]);
-                label.innerHTML = produit.colors[option.value];
-                
-                //définition de l'input
-                input.id = produit.colors[option.value];
-                input.name = produit.colors[option.value];
-                input.setAttribute('key', option.value);  //servira de controle pour savoir la couleur a déjà été sélectionnée
-                input.type='number';
-                input.setAttribute('min',0);
-                commande.appendChild(label);
-                commande.appendChild(input);
-                storage('showing_' + produit._id, showingProduct);
-
+                optionMaker(produit,option);             
                 //click sur le bouton annuler
                 cancellation();
             })
@@ -155,9 +44,124 @@ function product(param_fetchdata,param_idproduct) {
     }
 }
 
+const productShowing=(param_copy,param_produit,param_body)=>{
+    let showingProduct = { 'id': '', 'name': '', 'price': '', 'option': [] };
+    let name = select(param_copy, '.cardModelName')[0];
+    let option = select(param_copy, '.cardOptionSelect')[0];
+    let price = select(param_copy, '.cardPrice')[0];
+    let detail = select(param_copy, '.cardDetails')[0];
+    let id = select(param_copy, '.cardId')[0];
+    let image = select(param_copy, '.cardImage')[0];
+    let imageAlt = '';
+    let i = 0;
 
-const cancellation=()=>{
-    getId('cancel').addEventListener('click',()=>{
+    image.setAttribute('src', param_produit.imageUrl);
+    name.textContent = param_produit.name;
+    price.textContent = (param_produit.price) / 100;
+    detail.textContent = 'description: ' + param_produit.description;
+    id.value = param_produit._id;
+
+    let tableOption = [];
+    if (type == 'teddies') {
+        imageAlt = 'un ours en peluche';
+        for (let colors of param_produit.colors) {
+            let selectoption = document.createElement('option');
+            selectoption.value = i;
+            selectoption.textContent = colors;
+            option.appendChild(selectoption);
+            tableOption.push({ 'color': colors, 'quantity': '' });
+            i++;
+        }
+    }
+    else if (type == 'cameras') {
+        imageAlt = 'un appareil photo'
+        for (let lense of produit.lenses) {
+            let selectoption = document.createElement('option');
+            selectoption.value = i;
+            selectoption.textContent = lense;
+            option.appendChild(selectoption);
+            tableOption.push({ 'Lense': lense, 'quantity': '' });
+            i++;
+        }
+    }
+    else if (type == 'furniture') {
+        option.textContent = 'vernis: ' + param_produit.varnish;
+        imageAlt = 'un meuble';
+        for (let varnishOption of param_produit.varnish) {
+            let selectoption = document.createElement('option');
+            selectoption.value = i;
+            selectoption.textContent = varnishOption;
+            option.appendChild(selectoption);
+            tableOption.push({ 'Vernis': varnishOption, 'quantity': '' });
+            i++;
+        }
+    }
+
+    showingProduct.price = param_produit.price;
+    showingProduct.name = param_produit.name;
+    showingProduct.id = param_produit.id;
+    showingProduct.option = tableOption;
+
+    image.setAttribute('alt', imageAlt);
+    param_body.appendChild(param_copy); //on crée la carte produit
+
+}
+
+const optionMaker=(param_produit,param_option)=>{
+    let label = document.createElement('Label');
+    let input = document.createElement('input');
+    let commande = getClass('commande')[0];
+
+    //définition du label
+    label.setAttribute('for', param_produit.colors[param_option.value]);
+    label.innerHTML = param_produit.colors[param_option.value];
+
+    //définition de l'input
+    input.id = param_produit.colors[param_option.value];
+    input.name = param_produit.colors[param_option.value];
+    input.setAttribute('key', param_option.value);  //servira de controle pour savoir la couleur a déjà été sélectionnée
+    input.type = 'number';
+    input.setAttribute('min', 0);
+    input.value = 1;
+    commande.appendChild(label);
+    commande.appendChild(input);
+    storage('showing_' + param_produit._id, showingProduct);
+}
+
+const productData=(param_copy,param_produit,param_body)=>{
+    let url = select(param_copy, '.cardLink')[0];
+    let name = select(param_copy, '.cardModelName')[0];
+    let option = select(param_copy, '.cardOption')[0];
+    let price = select(param_copy, '.cardPrice')[0];
+    let detail = select(param_copy, '.cardDetails')[0];
+    let id = select(param_copy, '.cardId')[0];
+    let image = document.createElement('img');
+
+    image.setAttribute('alt', param_produit.alt);
+    image.setAttribute('src', param_produit.imageUrl);
+    url.href = `product.html?id=${param_produit._id}`;
+    name.textContent = 'modèle: ' + param_produit.name;
+    price.textContent = 'prix: ' + (param_produit.price) / 100 + '€';
+    detail.textContent = 'description: ' + param_produit.description;
+    id.textContent = param_produit._id;
+
+    if (type == 'teddies') {
+        option.textContent = 'couleurs: ' + param_produit.colors;
+    }
+    else if (type == 'cameras') {
+        option.textContent = 'objectifs: ' + param_produit.lenses;
+    }
+    else if (type == 'furniture') {
+        option.textContent = 'vernis: ' + param_produit.varnish;
+    }
+
+
+    url.appendChild(image); //on crée l'image dans le l'url
+    param_body.appendChild(param_copy); //on crée la carte produit
+}
+
+const cancellation = () => {
+    getId('cancel').addEventListener('click', () => {
         preventDefault();
         getType('input').remove;
     })
