46434f1ecdcebc25c1258756f300bb5acf844631
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-28 23:58:22 +0200

    controle des fonctions génériques pour le plan de test

--- frontend/javascript/script_generique_valid.js
+++ frontend/javascript/script_generique_valid.js
@@ -56,7 +56,41 @@ const mistake = (param_error) => {
     }
 }
 
+//le bouton panier : compter le nombre de produits
+const compteProduitsDuPanier = (param_fetchdata) => {
+    let count = 0;
+    for (let produit of param_fetchdata) {
+        if (getItem(produit._id)) {
+            count++;
+        }
+    }
+    afficherLePanier(count);
+}
 
+//le bouton panier : afficher le panier s'il n'est pas vide
+const afficherLePanier = (param_totalPanier) => {
+    if (param_totalPanier <= 0) {
+        textLook.textContent = 'Panier vide';
+        look.title = 'panier vide, bouton de panier neutralisé';
+        look.addEventListener('click', function (e) {
+            e.preventDefault();
+        })
+    }
+    else {
+        let span = document.createElement('span');
+        look.title = 'afficher le panier';
+        span.textContent = `Le panier contient :${param_totalPanier} produits`
+        textLook.appendChild(span);
+    }
+}
+
+//pour récupérer une valeur dans l'URL
+const getInUrl = (param_searched) => {
+    let place = window.location.search;             //on récupére ce qu'il y a après l'url standard. à partir du ?
+    const urlParams = new URLSearchParams(place);   //on crée une recherche dans le paramètre
+    let value = urlParams.get(param_searched);      //on récupère la valeur du parametre
+    return value;
+}
 
 //variables globales
 //  le bouton panier
