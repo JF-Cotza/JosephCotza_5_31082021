ea08e71d5e9bc6946e3a8e3a70672d2004e65eca
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-09-29 12:42:01 +0200

    script call

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -54,26 +54,4 @@ if (toLocalstorage){
     
 }
 
-
-
 //testingFunction();
-
-
-/*********************** application lancées depuis les pages ***********************/
-//index.html
-if (getId('allProducts')) {
-    console.log('allProducts');
-    fetchIndex();
-};
-
-//product.html
-if (getId('productCommand')){
-    let value=getInUrl('id');
-    fetchProduct(value);
-}; 
-
-//panier.html
-if (getId('panier')){
-    fetchAffichagePanier();
-}
-
---
+++ frontend/javascript/script_call.js
@@ -0,0 +1,17 @@
+/*********************** application lancées depuis les pages ***********************/
+//index.html
+if (getId('allProducts')) {
+    console.log('allProducts');
+    fetchIndex();
+};
+
+//product.html
+if (getId('productCommand')) {
+    let value = getInUrl('id');
+    fetchProduct(value);
+};
+
+//panier.html
+if (getId('panier')) {
+    fetchAffichagePanier();
+}
