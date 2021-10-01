80622e515b4107b53c97b68471d3cd28e57f0a7b
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-01 15:19:09 +0200

    compte le nombre de required vide / les required plein

--- frontend/javascript/script_template.js
+++ frontend/javascript/script_template.js
@@ -13,20 +13,22 @@ const totalPanier=()=>{
 
 const checkrequired=()=>{
     let inputs = getType('input');
-    let count = 0;
+    let countFalse = 0;
+    let countTrue=0;
     for (let item of inputs){
         if (getAttribute(item, 'required')==false){
             console.log('check required');
-            count++;
+            countFalse++;
         }
         else if (getAttribute(item, 'required') == true) {
             console.log('pas de check required');
+            countTrue++;
         }
         else{
             console.log('pas de required détecté')
         }
     }
-    console.log('count required '+count);
+    console.log('count required :'+countFalse+' not required :'+countTrue);
 }
 
 
