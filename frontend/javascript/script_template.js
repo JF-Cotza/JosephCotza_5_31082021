219933c11c13c7453912f59d598378874345c236
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-02 22:17:03 +0200

    get costumers datas

--- frontend/javascript/script_template.js
+++ frontend/javascript/script_template.js
@@ -1,10 +1,9 @@
 const totalPanier=()=>{
     let quantity=getClass('quantity');
     let singlePricePanier = getClass('singlePricePanier ');
-    let idArray=[];
     for (let input of  quantity )
     {
-        if(input.value>0){
+        if(input.value>0){ //élimine l'entête de tableau
             console.log('id:' + getAttribute(input, 'keyId') + ' option '+getAttribute(input, 'keyOption')+' qty '+input.value)
             
         }
