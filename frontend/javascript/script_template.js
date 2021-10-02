542131fd6a370d2b36f42819f4d0b8032eeed9d6
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-02 23:58:47 +0200

    test panier 18

--- frontend/javascript/script_template.js
+++ frontend/javascript/script_template.js
@@ -2,6 +2,7 @@
 const majPanier=()=>{
     let panier=getId('totalPanier');
     let inputs = getClass('optionQuantityPanier');
+    console.log(inputs);
     for(let item of inputs){
         item.addEventListener('change',()=>{
             panier.textContent=totalPanier();
