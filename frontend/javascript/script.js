4a6ae6c2727fef3bb8f256048a53aa88004d8d5a
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-07 01:29:10 +0200

    rectif  fetch x8

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -24,27 +24,9 @@ if (toConfirmation) {
 };
 
 const fetchCustomer = () => {
-    let contacting = getItem('customer');
-
-
-    
+    let contacting = getItem('customer');   
     let productsend = getItem('id');
     
-
-    /*let toSend={'contact':contact,'products':products}
-    let bodyConst = JSON.stringify(toSend); //on stringifie toSend
-    
-    console.log(testmess+' toSend '+toSend.contact+' '+toSend.products );
-    console.log('bodyConst: '+bodyConst);
-
-    !req.body.contact.firstName ||
-    !req.body.contact.lastName ||
-    !req.body.contact.address ||
-    !req.body.contact.city ||
-    !req.body.contact.email ||
-    !req.body.products) {
-
-    */
     fetch(apiLink + '/order', {
         method: "POST",
         headers: {
@@ -53,16 +35,15 @@ const fetchCustomer = () => {
         },
         body:
             JSON.stringify({ contact: contacting, products: productsend })
-    })
+        })
         .then((res) => {
             //window.location.href = "./confirmation.html";
             console.log(res.json());
             return res.json()
         })
         .then((value)=>{
-            console.log('produits: ' + value.products)
+            console.log('produits: ' + value.orderId)
         })
-
         .catch((error) => { console.log('catch: ' + error.message) })
     
 
@@ -75,4 +56,4 @@ const confirmFill = () => {
 }
 
 
-console.log('check storage x7');
+console.log('check storage x8');
