dcd0fa3f7a1e096e292378523b11d1ce7033a47e
Joseph Cotza <joseph.cotza.01@gmail.com>
2021-10-08 22:20:29 +0200

    test redirect async 9

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -61,7 +61,7 @@ const fetchCustomer = () => {
 
 }
 
-const storeDataReturn= async (param_data,param_amount)=>{
+const storeDataReturn= (param_data,param_amount)=>{
 
      storage('products ', param_data.products);//data.products est un array qui contient les infos du produit
      storage('contact ', param_data.contact); //data.contact contient les infos client
@@ -93,4 +93,4 @@ const redirect=()=>{
 }
 
 
-console.log('check storage x45')
+console.log('check storage x46')
